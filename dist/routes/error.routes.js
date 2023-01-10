24a8ae30a6dc9169583d92e95c6f90503f0cb9bd
Sean Christian Lozana <seanchristian.lozana@stratpoint.com>
2023-01-10 02:13:14 +0000

    Deleted dist/config/index.js, dist/config/index.js.map, dist/config/multer.js, dist/config/multer.js.map, dist/config/sequelize.js, dist/config/sequelize.js.map, dist/controllers/auth.controller.js, dist/controllers/auth.controller.js.map, dist/controllers/blog.controller.js, dist/controllers/blog.controller.js.map, dist/controllers/error.controller.js, dist/controllers/error.controller.js.map, dist/controllers/index.js, dist/controllers/index.js.map, dist/controllers/user.controller.js, dist/controllers/user.controller.js.map, dist/middlewares/index.js, dist/middlewares/index.js.map, dist/middlewares/skipVerifyJwt.js, dist/middlewares/skipVerifyJwt.js.map, dist/middlewares/validation.js, dist/middlewares/validation.js.map, dist/middlewares/verifyJwt.js, dist/middlewares/verifyJwt.js.map, dist/middlewares/verifyRole.js, dist/middlewares/verifyRole.js.map, dist/models/blogs.model.js, dist/models/blogs.model.js.map, dist/models/index.js, dist/models/index.js.map, dist/models/token.model.js, dist/models/token.model.js.map, dist/models/user.model.js, dist/models/user.model.js.map, dist/routes/auth.routes.js, dist/routes/auth.routes.js.map, dist/routes/blog.routes.js, dist/routes/blog.routes.js.map, dist/routes/error.routes.js, dist/routes/error.routes.js.map, dist/routes/index.js, dist/routes/index.js.map, dist/routes/user.routes.js, dist/routes/user.routes.js.map, dist/utils/cors.js, dist/utils/cors.js.map, dist/utils/index.js, dist/utils/index.js.map, dist/utils/multer.js, dist/utils/multer.js.map, dist/utils/paginate.js, dist/utils/paginate.js.map, dist/utils/updateImage.js, dist/utils/updateImage.js.map, dist/server.js, dist/server.js.map

--- dist/routes/error.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("../controllers");
-const errorRouter = (0, express_1.Router)();
-errorRouter.use(controllers_1.notFound);
-exports.default = errorRouter;
-//# sourceMappingURL=error.routes.js.map
